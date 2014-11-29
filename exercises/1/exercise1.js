2f363b344a1ffb0a94ddf4676143b7ff66b6323b
Gabor Czene <gabor@czene.co.uk>
2014-11-29 17:53:57 +0000

    elso feladat

---
+++ exercises/1/exercise1.js
@@ -0,0 +1,13 @@
+/**
+ * exercise 1
+ *
+ */
+/*
+var buttons = document.**jol osszegyutjtom**,
+    i;
+
+for(i = 0; i < stb. ) {
+    itt egy gomb es hozzadom az eventet ami jol kialerteli a megoldast(0,1,2)
+}
+
+*/
